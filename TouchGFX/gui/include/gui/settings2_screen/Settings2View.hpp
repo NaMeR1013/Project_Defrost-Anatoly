09e836f4bd5943a3d425d9bf5fe6abf7f4437ade
NaMeR1013 <tolis90@yandex.ru>
2023-06-29 17:52:54 +0300

    Settings2 screen added

---
+++ TouchGFX/gui/include/gui/settings2_screen/Settings2View.hpp
@@ -0,0 +1,17 @@
+#ifndef SETTINGS2VIEW_HPP
+#define SETTINGS2VIEW_HPP
+
+#include <gui_generated/settings2_screen/Settings2ViewBase.hpp>
+#include <gui/settings2_screen/Settings2Presenter.hpp>
+
+class Settings2View : public Settings2ViewBase
+{
+public:
+    Settings2View();
+    virtual ~Settings2View() {}
+    virtual void setupScreen();
+    virtual void tearDownScreen();
+protected:
+};
+
+#endif // SETTINGS2VIEW_HPP
---
+++ TouchGFX/gui/src/settings2_screen/Settings2View.cpp
@@ -0,0 +1,16 @@
+#include <gui/settings2_screen/Settings2View.hpp>
+
+Settings2View::Settings2View()
+{
+
+}
+
+void Settings2View::setupScreen()
+{
+    Settings2ViewBase::setupScreen();
+}
+
+void Settings2View::tearDownScreen()
+{
+    Settings2ViewBase::tearDownScreen();
+}
