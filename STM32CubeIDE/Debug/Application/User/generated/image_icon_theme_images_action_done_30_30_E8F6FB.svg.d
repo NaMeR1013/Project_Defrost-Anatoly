460814c32fd1491f4b1d95b38797f6d77042f881
NaMeR1013 <tolis90@yandex.ru>
2023-07-05 16:46:20 +0300

    DebugIgnore

--- STM32CubeIDE/Debug/Application/User/generated/image_icon_theme_images_action_done_30_30_E8F6FB.svg.d
+++
@@ -1,4 +0,0 @@
-Application/User/generated/image_icon_theme_images_action_done_30_30_E8F6FB.svg.o: \
- D:/ST/Project_Defrost-Anatoly/TouchGFX/generated/images/src/__generated/image_icon_theme_images_action_done_30_30_E8F6FB.svg.cpp \
- ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
--- STM32CubeIDE/Debug/Drivers/BSP/Components/stmpe811.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/stmpe811.o: \
- D:/ST/Project_Defrost-Anatoly/Drivers/BSP/Components/stmpe811/stmpe811.c \
- D:/ST/Project_Defrost-Anatoly/Drivers/BSP/Components/stmpe811/stmpe811.h \
- d:\st\project_defrost-anatoly\drivers\bsp\components\common\ts.h \
- d:\st\project_defrost-anatoly\drivers\bsp\components\common\io.h
-D:/ST/Project_Defrost-Anatoly/Drivers/BSP/Components/stmpe811/stmpe811.h:
-d:\st\project_defrost-anatoly\drivers\bsp\components\common\ts.h:
-d:\st\project_defrost-anatoly\drivers\bsp\components\common\io.h:
